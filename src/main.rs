fcccf2619b315393f3149b47ff186b1c41b249ca
gorira <tatsuhiko.shigoto@gmail.com>
2025-03-08 19:38:10 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::hash::{Hash, Hasher};
+use std::str::Bytes;
+use sha1::{Sha1, Digest};
 use std::fs::File;
-use std::io::Read;
+
 
 fn main() {
+    let mut hasher = Sha1::new();
     struct BlobObject {
         content: String,
         content_length: usize,
@@ -15,6 +18,12 @@ fn main() {
     blob.content = std::fs::read_to_string("test.txt").unwrap();
     blob.content_length = blob.content.len();
 
-    println!("Content: {}", blob.content);
-    println!("Content Length: {}", blob.content_length);
+    let blob_string = "blob ".to_owned() + &blob.content_length.to_string() + "\0" + &blob.content;
+    let blob_b = blob_string.as_bytes();
+    
+    hasher.update(blob_b);
+    let result = hasher.finalize();
+
+    println!("blob {}\0{}", blob.content_length, blob.content);
+    println!("{}" , hex::encode(result));
 }
